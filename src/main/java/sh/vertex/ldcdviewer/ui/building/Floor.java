4718a92a093a0d6bd9fa862ddc15e35a1df1a3c0
RaiNote <Timothy.Lutz@gmx.de>
2019-01-30 18:43:20 +0100

    Update: Floor loads dynamicly

--- src/main/java/sh/vertex/ldcdviewer/ui/building/Floor.java
+++ src/main/java/sh/vertex/ldcdviewer/ui/building/Floor.java
@@ -1,13 +1,17 @@
 package sh.vertex.ldcdviewer.ui.building;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import javafx.scene.canvas.GraphicsContext;
 
+import java.io.*;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Main Floor Class, handles Rooms
- *
+ * <p>
  * created by paul on 2019-01-18 at 23:22
  */
 public class Floor {
@@ -18,13 +22,36 @@ public class Floor {
     public Floor(int floorNumber) {
         this.floorNumber = floorNumber;
         this.floorRooms = new ArrayList<>();
+        LoadFloorObjects();
     }
 
-    protected void registerRoom(String text, int x, int y, int w, int h) {
+    private void LoadFloorObjects() {
+        Type type = new TypeToken<List<FloorJsonObject>>() {
+        }.getType();
+
+        //Builtin Resource
+        InputStream inputStream = getClass().getResourceAsStream(String.format("/assets/floors/floor%d.json", floorNumber));
+        InputStreamReader inputReader = new InputStreamReader(inputStream);
+        List<FloorJsonObject> floorJsonObjects = (new Gson()).fromJson(inputReader, type);
+
+
+        //Local
+        //List<FloorJsonObject> floorJsonObjects = (new Gson()).fromJson(new InputStreamReader(new FileInputStream(String.format("assets\\floor%d.json", floorNumber))), type);
+
+        for (FloorJsonObject floorObject : floorJsonObjects) {
+            if (floorObject.getType() == 0)
+                registerRoom(floorObject.getName(), floorObject.getX(), floorObject.getY(), floorObject.getW(), floorObject.getH());
+            else if (floorObject.getType() == 1)
+                registerDecorative(floorObject.getName(), floorObject.getX(), floorObject.getY(), floorObject.getW(), floorObject.getH());
+            else System.out.printf("Undefined FloorObjectType: %d%n", floorObject.getType());
+        }
+    }
+
+    private void registerRoom(String text, int x, int y, int w, int h) {
         this.floorRooms.add(new Room(text, x, y, w, h, RoomState.NORMAL));
     }
 
-    protected void registerDecorative(String description, double x, double y, int w, int h) {
+    private void registerDecorative(String description, double x, double y, int w, int h) {
         this.floorRooms.add(new Room(description, x, y, w, h, RoomState.DECO));
     }
 
